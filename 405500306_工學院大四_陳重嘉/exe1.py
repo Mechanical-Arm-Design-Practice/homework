082d950abab31a37072b544306a9a419e8b850a7
furret112 <furret112@gmail.com>
2020-03-19 15:33:36 +0800

    my first homework

---
+++ 405500306_工學院大四_陳重嘉/exe1.py
@@ -0,0 +1,9 @@
+while 1:
+    num_input = input('plz input a interger: ')
+    print("")
+    
+    if num_input.isnumeric():
+        print(num_input) 
+    else:
+        print("not a interger!!!")
+    print("")
