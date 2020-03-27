08231bec10587c75d524fcc4771c725a260190c8
furret112 <furret112@gmail.com>
2020-03-27 17:27:36 +0800

    add HW5 & delet folder

--- 405500306_工學院大四_陳重嘉/exe1.py
+++
@@ -1,9 +0,0 @@
-while 1:
-    num_input = input('plz input a interger: ')
-    print("")
-    
-    if num_input.isnumeric():
-        print(num_input) 
-    else:
-        print("not a interger!!!")
-    print("")
