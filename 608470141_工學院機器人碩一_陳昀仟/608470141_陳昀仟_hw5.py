5c9bc584f0d4b201007e84104828368c0742d4a4
CHEN-YUN-CHIEN <ycpss91255@gmail.com>
2020-03-31 23:02:38 +0800

    交作業喇-2

--- 608470141_工學院機器人碩一_陳昀仟/608470141_陳昀仟_hw5.py
+++ 608470141_工學院機器人碩一_陳昀仟/608470141_陳昀仟_hw5.py
@@ -4,11 +4,14 @@
 # 試寫一個名為Student 的類別
 # 其中屬性包含:
 # name, gender, grades
+
 # 函數包含:
 # avg: 回傳grades list的平均值
 # add(grade): 新增成績到grades list中
 # fcount: 回傳不及格(<60)的總數
+
 # 分別將每個學生的成績平均、不及格的的數目印出
+
 # 寫一個名為top的類別函數:
 # 傳入值為多個學生物件(使用不定個數)
 # 將平均分數最高的學生回傳
@@ -25,15 +28,40 @@ class Student():
         if len(self.grades) != 0:
             for i in self.grades:
                 tmp += float(i)/len(self.grades)
-            print(tmp)
-        else:
-            print("Do not make jokes,grades is empty")
+        return tmp
 
-    def add(self,grade):
-        self.grades.append(grade)
+    def add(self, grade):
+        [self.grades.append(i) for i in grade]
+        # for i in grade:
+        #     self.grades.append(i)
 
-    def fcount(self)
+    def fcount(self):
+        tmp = 0
+        for i in self.grades:
+            tmp += 1 if(i < 60) else 0
+        return tmp
+
+    @classmethod
+    def top(cls, *student):
+        tmp =[]
+        for i in student:
+            tmp.append(i.avg())
+        print("最高分學生為：%s" %student[tmp.index(max(tmp))].name)
 
 a = Student()
+b = Student()
+c = Student()
 
-a.avg()
+a.name="a"
+b.name="b"
+c.name="c"
+
+a.add([100, 59, 60, 70, 80])
+b.add([87, 60, 30, 10, 20])
+c.add([99, 80, 10, 61, 77])
+
+print("a平均：%.1f,不及格：%d" % (a.avg(), a.fcount()))
+print("b平均：%.1f,不及格：%d" % (b.avg(), b.fcount()))
+print("c平均：%.1f,不及格：%d" % (c.avg(), c.fcount()))
+
+Student.top(a,b,c)
