f675e6bf87ead015e69886185338dccc6881c50b
andysudo <andy26aslc@gmail.com>
2020-04-01 12:38:15 +0800

    hw5 complete

--- 608470026_工學院機器人碩一_張博皓/hw5.py
+++ 608470026_工學院機器人碩一_張博皓/hw5.py
@@ -6,6 +6,7 @@ class student:
     def __init__(self,name,gender):
         self.name = name
         self.gender = gender
+        self.grades = []
 
     def add(self,grade):
         self.grades.append(grade)
@@ -24,8 +25,16 @@ class student:
                 temp+=1
         
         return temp
-    #def top(self):
+    def top(self,alldata):  
+        avgmax = 0
+        for loop in alldata.keys():
+            if alldata[loop].avg() > avgmax:
+                avgmax = alldata[loop].avg()
+                superstudent = loop
+        return superstudent
 
+
+alldata = {}
 while True:
     people = input("請輸入學生姓名")
     boygirl = input("請輸入學生性別")
@@ -34,17 +43,29 @@ while True:
     while True:
         score = int(input("請輸入成績"))
         temp.add(score)
-        stop = input("是否要繼續輸入成績(y/n)")
+        ynflag = True;
+        while ynflag:
+            stop = input("是否要繼續輸入成績(y/n)")
+            if stop == 'y' or stop == 'n':
+                ynflag = False
+            else:
+                ynflag = True
         if stop == 'n':
             break
 
-    alldata = {}
     alldata.setdefault(people,temp)
-    allstop = input("是否要輸入其他學生(y/n)")
+    ynflag = True
+    while ynflag:
+        allstop = input("是否要輸入其他學生(y/n)")
+        if allstop == 'y' or allstop == 'n':
+            ynflag = False
+        else:
+            ynflag = True
     if allstop == 'n':
         break
 
-print(alldata[people].grades)
+for loop in alldata.keys():
+    print(alldata[loop].name,"的成績平均為",alldata[loop].avg(),",不及格的有",alldata[loop].fcount(),"科")
 
-print(alldata[people].avg())
-print("不及格的有",alldata[people].fcount())
+print("平均最高的為",temp.top(alldata))
+
